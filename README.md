ce5883911aa8ca10a4f4773373f293320b9b8201
Ibraheem Ahmed <ibraheem@ibraheem.ca>
2023-08-11 08:19:41 -0400

    add table of contents

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 The source code for [Learning Async Rust With Entirely Too Many Web Servers](https://ibraheem.ca/posts/learning-async-rust-with-too-many-web-servers/).
 
 Every iteration of the web server is a standalone binary in the `src/bin` folder.
+
+- [A Simple Web Server](https://github.com/ibraheemdev/too-many-web-servers/blob/master/src/bin/simple.rs)
+- [A Multithreaded Server](https://github.com/ibraheemdev/too-many-web-servers/blob/master/src/bin/multithreaded.rs)
+- [A Non-Blocking Server](https://github.com/ibraheemdev/too-many-web-servers/blob/master/src/bin/non_blocking.rs)
+- [A Multiplexed Server](https://github.com/ibraheemdev/too-many-web-servers/blob/master/src/bin/multiplexed.rs)
+- [An Async Server](https://github.com/ibraheemdev/too-many-web-servers/blob/master/src/bin/asynchronous.rs)
+- [A Functional Server](https://github.com/ibraheemdev/too-many-web-servers/blob/master/src/bin/functional.rs)
+- [A Graceful Server](https://github.com/ibraheemdev/too-many-web-servers/blob/master/src/bin/graceful.rs)
+- [A Tokio Server](https://github.com/ibraheemdev/too-many-web-servers/blob/master/src/bin/tokio.rs)
---
+++ src/bin/multithreaded.rs
@@ -0,0 +1,75 @@
+use std::{
+    io::{self, Read, Write},
+    net::{TcpListener, TcpStream},
+};
+
+fn main() {
+    let listener = TcpListener::bind("localhost:3000").unwrap();
+
+    loop {
+        let (connection, _) = listener.accept().unwrap();
+
+        std::thread::spawn(move || {
+            if let Err(e) = handle_connection(connection) {
+                println!("failed to handle connection: {e}")
+            }
+        });
+    }
+}
+
+fn handle_connection(mut connection: TcpStream) -> io::Result<()> {
+    let mut read = 0;
+    let mut request = [0u8; 1024];
+
+    loop {
+        // try reading from the stream
+        let num_bytes = connection.read(&mut request[read..])?;
+
+        // the client disconnected
+        if num_bytes == 0 {
+            println!("client disconnected unexpectedly");
+            return Ok(());
+        }
+
+        // keep track of how many bytes we've read
+        read += num_bytes;
+
+        // have we reached the end of the request?
+        if request.get(read - 4..read) == Some(b"\r\n\r\n") {
+            break;
+        }
+    }
+
+    let request = String::from_utf8_lossy(&request[..read]);
+    println!("{request}");
+
+    // "Hello World!" in HTTP
+    let response = concat!(
+        "HTTP/1.1 200 OK\r\n",
+        "Content-Length: 12\n",
+        "Connection: close\r\n\r\n",
+        "Hello world!"
+    );
+
+    let mut written = 0;
+
+    loop {
+        // write the remaining response bytes
+        let num_bytes = connection.write(response[written..].as_bytes())?;
+
+        // the client disconnected
+        if num_bytes == 0 {
+            println!("client disconnected unexpectedly");
+            return Ok(());
+        }
+
+        written += num_bytes;
+
+        // have we written the whole response yet?
+        if written == response.len() {
+            break;
+        }
+    }
+
+    connection.flush()
+}
